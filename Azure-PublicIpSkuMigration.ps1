c8350699bab355716f5e00fdd16aebc32c7d65b2
slowbrowse <slowbrowse@proton.me>
2025-06-24 10:46:52 +0200

    Add test comment

--- Azure-PublicIpSkuMigration.ps1
+++ Azure-PublicIpSkuMigration.ps1
@@ -7,3 +7,4 @@ Start-VMPublicIPUpgrade -VMName 'xtdev02-vm' -ResourceGroupName 'xtdev-rg' -What
 
 Start-VMPublicIPUpgrade -VMName 'xtdev01-vm' -ResourceGroupName 'xtdev-rg'
 Start-VMPublicIPUpgrade -VMName 'xtdev02-vm' -ResourceGroupName 'xtdev-rg'
+# Test
