ccf77be9ad3c992c4982fcab1aefd828c910fe7c
slowbrowse <slowbrowse@proton.me>
2025-06-26 15:32:56 +0200

    Will this ever work

--- Azure-PublicIpSkuMigration.ps1
+++ Azure-PublicIpSkuMigration.ps1
@@ -7,4 +7,3 @@ Start-VMPublicIPUpgrade -VMName 'xtdev02-vm' -ResourceGroupName 'xtdev-rg' -What
 
 Start-VMPublicIPUpgrade -VMName 'xtdev01-vm' -ResourceGroupName 'xtdev-rg'
 Start-VMPublicIPUpgrade -VMName 'xtdev02-vm' -ResourceGroupName 'xtdev-rg'
-# testing123456
