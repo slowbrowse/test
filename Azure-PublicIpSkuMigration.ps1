ae88d9dd7e7fa1d0ddc7458e79f6b4a1ac74c4aa
slowbrowse <50610197+slowbrowse@users.noreply.github.com>
2025-06-27 09:05:25 +0200

    Update Azure-PublicIpSkuMigration.ps1

--- Azure-PublicIpSkuMigration.ps1
+++ Azure-PublicIpSkuMigration.ps1
@@ -7,4 +7,4 @@ Start-VMPublicIPUpgrade -VMName 'xtdev02-vm' -ResourceGroupName 'xtdev-rg' -What
 
 Start-VMPublicIPUpgrade -VMName 'xtdev01-vm' -ResourceGroupName 'xtdev-rg'
 Start-VMPublicIPUpgrade -VMName 'xtdev02-vm' -ResourceGroupName 'xtdev-rg'
-# Hopefully this will trigger me
+# Getting bored of this
